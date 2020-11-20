28f0844c5dc5be321c32bd0670e9d4ff1f7f07e9
Chris Patrick <atomize101@gmail.com>
2020-11-20 08:29:57 -0500

    Exercise 1.3 complete. Refactor to change data into objects

--- src/components/Content.js
+++ src/components/Content.js
@@ -5,8 +5,8 @@ const Content = ({ part1, part2, part3, exercises1, exercises2, exercises3 }) =>
 	return (
 		<div>
 			<Part part1={part1} exercises1={exercises1} />
-			<Part part1={part1} exercises1={exercises1} />
-			<Part part1={part1} exercises1={exercises1} />
+			<Part part2={part2} exercises2={exercises2} />
+			<Part part3={part3} exercises3={exercises3} />
 		</div>
 	);
 };
