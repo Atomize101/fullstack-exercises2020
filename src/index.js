7548905fde99b8be6501743021b1b0f311a1512a
Chris Patrick <atomize101@gmail.com>
2020-11-22 13:42:35 -0500

    Step 1.14 complete. Added a TopAnecdote component to show the most voted anecdote.

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 
 const App = (props) => {
 	const [selected, setSelected] = useState(0);
+	// Create an array of how many anecdotes and set each value to 0 initially.
 	const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
 
 	const getRandomAnecdote = () => {
@@ -11,19 +12,34 @@ const App = (props) => {
 	};
 
 	const vote = () => {
+		// Create a new array to store the votes being selected, then update state
 		const newVotes = [...votes];
 		newVotes[selected]++;
 		console.log(votes);
 		setVotes(newVotes);
 	};
 
+	const mostVotes = votes.reduce((a, b, i, arr) => (b > arr[a] ? i : a), 0);
+
+	const TopAnecdote = () => {
+		return (
+			<div>
+				<h1>Anecdote with the most votes</h1>
+				{props.anecdotes[mostVotes]}
+			</div>
+		);
+	};
+
 	return (
 		<div>
+			<h1>Anecdote of the day</h1>
 			{props.anecdotes[selected]}
 			<br />
 			<p>This anecdote has {votes[selected]} votes</p>
 			<button onClick={vote}>Vote</button>
 			<button onClick={getRandomAnecdote}>Test</button>
+			<br />
+			<TopAnecdote />
 		</div>
 	);
 };
