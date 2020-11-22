d01990b8c6347fffa3d2703ac03ab32e9f72e238
Chris Patrick <atomize101@gmail.com>
2020-11-22 09:14:53 -0500

    Finished exercise 1.6

--- src/index.js
+++ src/index.js
@@ -7,7 +7,24 @@ const App = () => {
 	const [neutral, setNeutral] = useState(0);
 	const [bad, setBad] = useState(0);
 
-	return <div>code here</div>;
+	return (
+		<div>
+			<h1>Give Feedback</h1>
+			<div>
+				<button onClick={() => setGood(good + 1)}>Good</button>
+				<button onClick={() => setNeutral(neutral + 1)}>Neutral</button>
+				<button onClick={() => setBad(bad + 1)}>Bad</button>
+			</div>
+			<div>
+				<h1>Statistics</h1>
+			</div>
+			<div>
+				<p>good: {good}</p>
+				<p>neutral: {neutral}</p>
+				<p>bad: {bad}</p>
+			</div>
+		</div>
+	);
 };
 
 ReactDOM.render(<App />, document.getElementById('root'));
