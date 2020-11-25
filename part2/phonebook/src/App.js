72ea43f1eea2448e8d146ce324860ea6463f5c0f
Chris Patrick <atomize101@gmail.com>
2020-11-25 08:35:33 -0500

    Exercise 2.8 complete. But data now showing the correct way

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,17 @@
 import React, { useState } from 'react';
 
 const App = () => {
-	const [persons, setPersons] = useState([{ name: 'Arto Hellas' }, { name: 'Chris Patrick' }]);
+	const [persons, setPersons] = useState([{ name: '' }]);
 	const [newName, setNewName] = useState('');
-	const [showAll, setShowAll] = useState(true);
+	const [numbers, setNumbers] = useState([{ number: '' }]);
+	const [newNumber, setNewNumber] = useState('');
 
 	const addPerson = (event) => {
 		event.preventDefault();
 		console.log('button clicked', event.target);
 		setPersons(persons.concat({ name: newName }));
 		setNewName('');
+		setNumbers(numbers.concat({ number: newNumber }));
 	};
 
 	const handleNameChange = (event) => {
@@ -17,27 +19,39 @@ const App = () => {
 		setNewName(event.target.value);
 	};
 
-	const numbers = () => persons.map((person, i) => <p key={i}>{person.name}</p>);
+	const handleNumberChange = (event) => {
+		console.log(event.target.vale);
+		setNewNumber(event.target.value);
+	};
+
+	const allPeople = () => persons.map((person, i) => <p key={i}>{person.name}</p>);
+	const allNumbers = () => numbers.map((number, i) => <p key={i}>{number.number}</p>);
 
 	const personExists = !!persons.find((p) => p.name.toLowerCase() === newName.toLowerCase());
 
-	if (personExists) {
-		alert(`${newName} is already added to the phone book`);
-		return;
-	}
+	//if (personExists) {
+	//	alert(`${newName} is already added to the phone book`);
+	//	return;
+	//}
 	return (
 		<div>
 			<h2>Phonebook</h2>
 			<form onSubmit={addPerson}>
 				<div>
-					name: <input value={newName} onChange={handleNameChange} />
+					Name: <input value={newName} onChange={handleNameChange} />
+				</div>
+				<div>
+					Number: <input value={newNumber} onChange={handleNumberChange} />
 				</div>
 				<div>
 					<button type="submit">add</button>
 				</div>
 			</form>
 			<h2>Numbers</h2>
-			<h3>{numbers()}</h3>
+			<h3>
+				{allPeople()}
+				{allNumbers()}
+			</h3>
 		</div>
 	);
 };
